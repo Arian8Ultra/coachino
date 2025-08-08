fd5ab736ca57f2ef5cf3d598a79a392f98fb8061
Arian Rezaei <arian8ultra@gmail.com>
2025-08-09 03:04:00 +0330

    feat: add ChartContainer and related components for enhanced chart functionality

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,8 +11,7 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
-        accent:
-          "bg-accent text-accent-foreground shadow-xs hover:bg-accent/90",
+        accent: "bg-accent text-accent-foreground shadow-xs hover:bg-accent/90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -26,6 +25,8 @@ const buttonVariants = cva(
           "bg-primary/15 backdrop-blur-lg border text-primary hover:bg-primary/20 dark:bg-black/10 dark:border-black/20 dark:text-white w-full",
         accentGlass:
           "bg-accent/15 backdrop-blur-lg border text-accent hover:bg-accent/20  dark:border-black/20 dark:text-white w-full",
+        successGlass:
+          "bg-green-500/15 backdrop-blur-lg border text-green-700 hover:bg-green-500/20  dark:border-black/20 dark:text-white w-full",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
