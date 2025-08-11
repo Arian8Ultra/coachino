a2ec82895668dd25412a6d84255f4b241a910a01
Arian Rezaei <arian8ultra@gmail.com>
2025-08-11 23:49:20 +0330

    refactor: update layout and page components for improved structure and styling

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,13 +7,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Image
         src={"/backgrounds/blurgradient.svg"}
         alt='Page Top Background'
-        className='fixed top-0 left-0 w-full h-screen object-cover opacity-30 brightness-100 dark:brightness-100 -z-10'
+        className='fixed top-0 left-0 w-full h-screen object-cover brightness-100 dark:brightness-100 dark:saturate-[75%] contrast-125 -z-10 opacity-20'
         width={1920}
         height={1080}
       />
       <LandingTovNav />
-
-      {children}
+      <div className='relative p-5'>{children}</div>
     </section>
   );
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,68 +1,16 @@
-import LandingGridItem from "@/components/layout/Landing/LandingParts/LandingGrid/LandingGridItem";
-import LandingSlider from "@/components/layout/Landing/LandingSlider/LandingSlider";
-import Image from "next/image";
+import HomeCapsule from "@/components/main/Capsule/HomeCapsule";
+import HomeCarousel from "@/components/main/Carousel/HomeCarousel";
+import HomeHeader from "@/components/main/header/HomeHeader";
+import HomeStartNow from "@/components/main/StartNow/HomeStartNow";
 
 export default function Page() {
   return (
-    <div className='flex flex-col gap-5 p-5 md:p-50 snap-always snap-y snap-mandatory'>
-      <LandingSlider className='max-h-[80dvh] md:max-h-max' />
-      <div className='flex flex-wrap gap-24 my-20 snap-always snap-y snap-mandatory'>
-        <LandingGridItem className='w-full flex md:flex-row flex-col not-only-of-type:items-center justify-between'>
-          <div className='flex flex-col gap-3 h-full justify-evenly'>
-            <h2 className='md:text-7xl text-3xl font-bold'>انگیزهٔ روزانه</h2>
-            <p className='text-gray-600 dark:text-gray-400 text-2xl md:max-w-2/3 *:leading-8 text-justify'>
-              هر روز با نقل‌قول‌ها و نکته‌های انگیزشی الهام بگیرید تا در مسیر
-              بمانید.
-            </p>
-          </div>
-          <Image
-            src={"/landing/daily-motivation.png"}
-            alt='Daily Motivation'
-            className='w-full md:w-1/2 object-cover aspect-video md:aspect-[1.5/1] rounded-lg'
-            width={1000}
-            height={1000}
-          />
-        </LandingGridItem>
-
-        <LandingGridItem className='w-full flex md:flex-row flex-col not-only-of-type:items-center justify-between'>
-          <div className='flex flex-col gap-3 h-full justify-evenly'>
-            <h2 className='md:text-7xl text-3xl font-bold'>
-              هدف خود را تعیین کنید
-            </h2>
-            <p className='text-gray-600 dark:text-gray-400 text-2xl md:max-w-2/3 *:leading-8 text-justify'>
-              اهداف مبهم خود را به هدفی شفاف و دقیق تبدیل کنید. کوچینو به شما
-              کمک می‌کند نقاط عطف قابل‌سنجش تعیین کنید و همان لحظه نخستین گام
-              عملی را پیش رویتان بگذارد.
-            </p>
-          </div>
-          <Image
-            src={"/landing/set-your-goal.png"}
-            alt='Daily Motivation'
-            className='w-full md:w-1/2 object-cover aspect-video md:aspect-[1.5/1] rounded-lg'
-            width={1000}
-            height={1000}
-          />
-        </LandingGridItem>
-        <LandingGridItem className='w-full flex md:flex-row flex-col not-only-of-type:items-center justify-between'>
-          <div className='flex flex-col gap-3 h-full justify-evenly'>
-            <h2 className='md:text-7xl text-3xl font-bold'>
-              پیشرفت خود را پیگیری کنید
-            </h2>
-            <p className='text-gray-600 dark:text-gray-400 text-2xl md:max-w-2/3 *:leading-8 text-justify'>
-              رشد پی‌درپی رکوردهایتان را تماشا کنید، نقاط عطف را ببینید که روشن
-              می‌شوند و روندها را در لحظه تغییر می‌کنند. بازخورد بصری شما را
-              صادق، متمرکز و مشتاق به پیشروی بیشتر نگه می‌دارد.
-            </p>
-          </div>
-          <Image
-            src={"/landing/track-progress.png"}
-            alt='Daily Motivation'
-            className='w-full md:w-1/2 object-cover aspect-video md:aspect-[1.5/1] rounded-lg'
-            width={1000}
-            height={1000}
-          />
-        </LandingGridItem>
-      </div>
+    <div className='flex flex-col gap-20 p-5 md:p-20 snap-always snap-y snap-mandatory'>
+      {/* <LandingSlider className='max-h-[80dvh] md:max-h-max' /> */}
+      <HomeHeader />
+      <HomeCapsule />
+      <HomeCarousel />
+      <HomeStartNow />
     </div>
   );
 }
