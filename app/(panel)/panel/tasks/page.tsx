4a3c18f74b3a740b031f3a9be8fd8d92f64acbb3
Arian Rezaei <arian8ultra@gmail.com>
2025-08-11 09:22:42 +0330

    feat: update task grid layout for responsive design

--- app/(panel)/panel/tasks/page.tsx
+++ app/(panel)/panel/tasks/page.tsx
@@ -72,7 +72,7 @@ export default async function Page() {
         h1='تسک ها'
         className='mb-4'
       />
-      <div className='grid grid-cols-2 gap-4'>
+      <div className='grid md:grid-cols-2 gap-4'>
         {userTasks?.sort((a, b) => {
             return (
               (a.dueDate &&
