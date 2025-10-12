c5c2567d256705faec4ad83fecdeb309e13395a8
Arian Rezaei <arian8ultra@gmail.com>
2025-10-12 13:26:57 +0330

    refactor: update layout structure and component imports in Page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,16 +1,22 @@
 import Footer from "@/components/layout/Landing/Footer/Footer";
 import HomeAboutUs from "@/components/main/home/HomeAboutUs";
-import HomeStats from "@/components/main/home/HomeStats";
-import NewHomeHeader from "@/components/main/home/NewHomeHeader";
+import HomeCTOCard from "@/components/main/home/HomeCTOCard";
+import NewHomeHero from "@/components/main/home/NewHomeHero";
+import NewHomeStats from "@/components/main/home/NewHomeStats";
+import NewWhyCoachino from "@/components/main/home/NewWhyCoachino";
 
 export default function Page() {
   return (
-    <div className='flex flex-col gap-10 '>
+    <div className='flex flex-col md:gap-20 gap-10 '>
       {/* <LandingSlider className='max-h-[80dvh] md:max-h-max' /> */}
-      <NewHomeHeader />
-      <HomeStats />
-      <HomeAboutUs/>
-      <Footer/>
+      <NewHomeHero />
+      <NewHomeStats />
+      <NewWhyCoachino />
+      <HomeCTOCard />
+      {/* <NewHomeHeader /> */}
+      {/* <HomeStats /> */}
+      <HomeAboutUs />
+      <Footer />
     </div>
   );
 }
