05e3d2fe55533fc454532be94aea3995bf20dc3d
Arian Rezaei <arian8ultra@gmail.com>
2025-10-21 15:17:06 +0330

    style(panel,layout): remove extra padding from main container

--- app/(panel)/panel/layout.tsx
+++ app/(panel)/panel/layout.tsx
@@ -31,7 +31,7 @@ export default async function MainLayout({
       <SidebarProvider>
         {/* <TopNav /> */}
         <MainSidebar user={user} />
-        <div className='flex-1 m-4 md:ms-8 rounded-lg p-2 ' id='main-container'>
+        <div className='flex-1 m-4 md:ms-8 rounded-lg ' id='main-container'>
           <NextTopLoader color='#2563eb' />
           <TopNav />
           {children}
