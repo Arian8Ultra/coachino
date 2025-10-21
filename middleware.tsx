c1269ec008ee8a04aa8b182b73af74ea48b2cad4
Arian Rezaei <arian8ultra@gmail.com>
2025-10-21 15:18:46 +0330

    feat(middleware,prisma,auth,style): add panel auth middleware; add iconName/backgroundUrl to Subscription; localize OTP SMS to Persian; format utils imports

---
+++ middleware.tsx
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// This function can be marked async if using await inside
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+  if (!token) {
+    const redirectUrl = new URL(
+      "/login?redirect=" + encodeURIComponent(request.url),
+      request.url,
+    );
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/panel/:path*",
+};
---
+++ prisma/migrations/20251021094816_v25_3/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "public"."Subscription" ADD COLUMN     "backgroundUrl" TEXT,
+ADD COLUMN     "iconName" TEXT;
