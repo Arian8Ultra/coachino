972b298ed4978344797f286cd31521737b4880f6
Arian Rezaei <arian8ultra@gmail.com>
2025-11-01 12:24:32 +0330

    feat(video,prisma): wrap VideoModal in <dialog> (bump z-index) and add referral fields to User + migration

--- components/layout/VideoModal/VideoModal.tsx
+++ components/layout/VideoModal/VideoModal.tsx
@@ -20,21 +20,22 @@ const VideoModal = ({
   onEnded,
 }: Props) => {
   return (
-    <motion.div
-      className={cn(
-        "absolute z-20 p-4 backdrop-blur-sm rounded-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-full h-screen ",
-        className,
-      )}
-      initial={{ opacity: 0, scale: 0.4 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.4 }}
-      transition={{ duration: 0.5 }}
-      whileInView={{
-        opacity: 1,
-        scale: 1,
-      }}
-    >
-      {/* <motion.div
+    <dialog open>
+      <motion.div
+        className={cn(
+          "absolute z-50 p-4 backdrop-blur-sm rounded-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-full h-screen ",
+          className,
+        )}
+        initial={{ opacity: 0, scale: 0.4 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.4 }}
+        transition={{ duration: 0.5 }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+        }}
+      >
+        {/* <motion.div
         className='absolute z-20 p-4 bg-glass backdrop-blur-md rounded-md  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center md:w-fit'
         initial={{ opacity: 0, scale: 0.4 }}
         whileInView={{
@@ -44,21 +45,22 @@ const VideoModal = ({
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }}
       > */}
-      <video
-        src={src}
-        controls={controls}
-        loop={loop}
-        muted={muted}
-        autoPlay={autoPlay}
-        onEnded={() => {
-          onEnded?.();
-        }}
-        className='h-full rounded-sm md:max-h-[80vh] w-screen md:w-auto object-contain'
-        width={2000}
-        height={2000}
-      />
-      {/* </motion.div> */}
-    </motion.div>
+        <video
+          src={src}
+          controls={controls}
+          loop={loop}
+          muted={muted}
+          autoPlay={autoPlay}
+          onEnded={() => {
+            onEnded?.();
+          }}
+          className='h-full rounded-sm md:max-h-[80vh] w-screen md:w-auto object-contain'
+          width={2000}
+          height={2000}
+        />
+        {/* </motion.div> */}
+      </motion.div>
+    </dialog>
   );
 };
 
