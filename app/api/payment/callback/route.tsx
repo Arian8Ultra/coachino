00f218fe6a1798e67f5f5545b2695019ea55cbae
Arian Rezaei <87231161+Arian8Ultra@users.noreply.github.com>
2026-01-06 15:52:32 +0330

    feat: refactor payment callback logic for improved readability and error handling

--- app/api/payment/callback/route.tsx
+++ app/api/payment/callback/route.tsx
@@ -3,99 +3,104 @@ import { prisma } from "@/prisma/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const success = url.searchParams.get("success");
-  const trackId = url.searchParams.get("trackId");
-  const orderId = url.searchParams.get("orderId");
-  const status = url.searchParams.get("status");
+  try {
+    const url = new URL(request.url);
+    const success = url.searchParams.get("success");
+    const trackId = url.searchParams.get("trackId");
+    const orderId = url.searchParams.get("orderId");
+    const status = url.searchParams.get("status");
 
-  const userTransaction = await prisma.transaction.findFirst({
-    where: {
-      id: orderId || "",
-      transactionId: trackId || "",
-    },
-  });
-  if (!userTransaction) {
-    return new Response("Transaction not found", { status: 404 });
-  }
-  if (success === "1") {
-    console.log("verifyZibalPayment");
-
-    const verifyResponse = await verifyZibalPayment({
-      trackId: Number(userTransaction.transactionId),
+    const userTransaction = await prisma.transaction.findFirst({
+      where: {
+        id: orderId || "",
+        transactionId: trackId || "",
+      },
     });
+    if (!userTransaction) {
+      return new Response("Transaction not found", { status: 404 });
+    }
+    if (success === "1") {
+      console.log("verifyZibalPayment");
 
-    if (verifyResponse.result !== 100) {
+      const verifyResponse = await verifyZibalPayment({
+        trackId: Number(userTransaction.transactionId),
+      });
+
+      if (verifyResponse.result !== 100 && verifyResponse.result !== 201) {
+        await prisma.transaction.update({
+          where: { id: userTransaction.id },
+          data: { status: "FAILED", zibalStatus: status || "" },
+        });
+        return new Response("Payment failed", { status: 400 });
+      }
+
+      if (userTransaction.subscriptionId) {
+        const subscription = await prisma.subscription.findUnique({
+          where: { id: userTransaction.subscriptionId },
+        });
+
+        if (subscription) {
+          const startDate = new Date();
+          const endDate = new Date(
+            new Date().setMonth(new Date().getMonth() + subscription.duration),
+          );
+          await prisma.userSubscription.updateMany({
+            where: { userId: userTransaction.userId },
+            data: {
+              isActive: false,
+            },
+          });
+          // then create a new one
+          await prisma.userSubscription.create({
+            data: {
+              userId: userTransaction.userId,
+              subscriptionId: subscription.id,
+              startDate,
+              endDate,
+            },
+          });
+        }
+      }
+
       await prisma.transaction.update({
-        where: { id: userTransaction.id },
-        data: { status: "FAILED", zibalStatus: status || "" },
+        where: {
+          id: userTransaction.id,
+        },
+        data: {
+          status: "VERIFIED",
+          zibalStatus: status || "",
+        },
+      });
+
+      await prisma.userDiscountCode.create({
+        data: {
+          userId: userTransaction.userId,
+          discountCodeId: userTransaction.userDiscountCodeId || "",
+          assignedAt: new Date(),
+        },
+      });
+      await prisma.discountCode.updateMany({
+        where: { id: userTransaction.userDiscountCodeId || "" },
+        data: {
+          numberOfUses: { increment: 1 },
+        },
+      });
+
+      return NextResponse.redirect(new URL("/panel/profile", request.url));
+    } else {
+      await prisma.transaction.update({
+        where: {
+          id: userTransaction.id,
+        },
+        data: {
+          status: "FAILED",
+          zibalStatus: status || "",
+        },
       });
       return new Response("Payment failed", { status: 400 });
     }
-
-    if (userTransaction.subscriptionId) {
-      const subscription = await prisma.subscription.findUnique({
-        where: { id: userTransaction.subscriptionId },
-      });
-
-      if (subscription) {
-        const startDate = new Date();
-        const endDate = new Date(
-          new Date().setMonth(new Date().getMonth() + subscription.duration),
-        );
-        await prisma.userSubscription.updateMany({
-          where: { userId: userTransaction.userId },
-          data: {
-            isActive: false,
-          },
-        });
-        // then create a new one
-        await prisma.userSubscription.create({
-          data: {
-            userId: userTransaction.userId,
-            subscriptionId: subscription.id,
-            startDate,
-            endDate,
-          },
-        });
-      }
-    }
-
-    await prisma.transaction.update({
-      where: {
-        id: userTransaction.id,
-      },
-      data: {
-        status: "VERIFIED",
-        zibalStatus: status || "",
-      },
-    });
-
-    await prisma.userDiscountCode.create({
-      data: {
-        userId: userTransaction.userId,
-        discountCodeId: userTransaction.userDiscountCodeId || "",
-        assignedAt: new Date(),
-      },
-    });
-    await prisma.discountCode.updateMany({
-      where: { id: userTransaction.userDiscountCodeId || "" },
-      data: {
-        numberOfUses: { increment: 1 },
-      },
-    });
-
-    return NextResponse.redirect(new URL("/panel/profile", request.url));
-  } else {
-    await prisma.transaction.update({
-      where: {
-        id: userTransaction.id,
-      },
-      data: {
-        status: "FAILED",
-        zibalStatus: status || "",
-      },
-    });
-    return new Response("Payment failed", { status: 400 });
+  } catch (error) {
+    console.error("Error in payment callback:", error);
+    return new Response("Internal Server Error", { status: 500 });
   }
 }
