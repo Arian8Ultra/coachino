c107212f59b96701f7088f5c6e8bb0d1f27ff180
Arian Rezaei <arian8ultra@gmail.com>
2026-01-06 12:21:55 +0330

    feat: add DiscountCode and UserDiscountCode functions for CRUD operations

--- app/api/payment/request/route.tsx
+++ app/api/payment/request/route.tsx
@@ -56,6 +56,8 @@ export async function POST(request: Request) {
       transactionId: "",
     },
   });
+
+  
   const response = await createZibalPaymentRequest({
     amount: finalAmount,
     callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/payment/callback`,
