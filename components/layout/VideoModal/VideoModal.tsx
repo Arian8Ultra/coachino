d198c16264f74b6477e1be3c996e5d3136a2e371
Arian Rezaei <87231161+Arian8Ultra@users.noreply.github.com>
2026-01-06 17:43:22 +0330

    feat: simplify VideoModal by removing Dialog component and redundant code

--- components/layout/VideoModal/VideoModal.tsx
+++ components/layout/VideoModal/VideoModal.tsx
@@ -20,60 +20,26 @@ const VideoModal = ({
 }: Props) => {
   const [bigPictureOpen, setBigPictureOpen] = useState(true);
   return (
-    <>
-      {/* <Dialog
-        open={bigPictureOpen}
-        onOpenChange={() => {
-          setBigPictureOpen(false);
+    <div
+      className={`${
+        !bigPictureOpen
+          ? "absolute bottom-4 left-4 w-16 h-16 rounded-full"
+          : "absolute inset-0 max-w-[70dvw] m-auto h-9/12 md:max-h-[80vh] aspect-square rounded-md "
+      } overflow-hidden shadow-lg cursor-pointer z-40 ${className || ""}`}
+      onClick={() => setBigPictureOpen(!bigPictureOpen)}
+    >
+      <video
+        src={src}
+        muted={muted}
+        autoPlay={!bigPictureOpen ? true : autoPlay}
+        loop={loop}
+        controls={!bigPictureOpen ? false : controls}
+        className='w-full h-full object-cover'
+        onEnded={() => {
+          onEnded?.();
         }}
-      >
-        <DialogContent
-          // showCloseButton={false}
-          className={`p-0  max-w-full  max-h-full  ${className || ""}`}
-        >
-          <DialogHeader className='hidden'>
-            <DialogTitle className='hidden'>ویدیو توضیح</DialogTitle>
-          </DialogHeader>
-          <video
-            src={src}
-            controls={controls}
-            loop={loop}
-            muted={muted}
-            autoPlay={autoPlay}
-            onEnded={() => {
-              onEnded?.();
-            }}
-            className='h-full rounded-sm md:max-h-[80vh] w-screen md:w-auto object-contain'
-            width={2000}
-            height={2000}
-          />
-        </DialogContent>
-      </Dialog> */}
-      {/* {!bigPictureOpen && ( */}
-      <div
-        className={`${
-          !bigPictureOpen
-            ? "absolute bottom-4 left-4 w-16 h-16 rounded-full"
-            : "absolute inset-0 max-w-[70dvw] m-auto h-9/12 md:max-h-[80vh] aspect-square rounded-md "
-        } overflow-hidden shadow-lg cursor-pointer z-40 ${className || ""}`}
-        onClick={() => setBigPictureOpen(!bigPictureOpen)}
-      >
-        <video
-          src={src}
-          muted={muted}
-          autoPlay={!bigPictureOpen ? true : autoPlay}
-          loop={loop}
-          controls={!bigPictureOpen ? false : controls}
-          className='w-full h-full object-cover'
-          onEnded={() => {
-            onEnded?.();
-          }}
-        />
-      </div>
-      {/* )} */}
-    </>
-
-    // </dialog>
+      />
+    </div>
   );
 };
 
