d58c4a0e79d379574765c9b9f00468715e46c72d
Arian Rezaei <87231161+Arian8Ultra@users.noreply.github.com>
2026-01-07 16:03:23 +0330

    feat: add TopTitle component to MBTI test page for enhanced user guidance

--- app/(main)/mbti/page.tsx
+++ app/(main)/mbti/page.tsx
@@ -1,4 +1,5 @@
 import LandingExamForum from "@/components/exam/LandingExamForum";
+import TopTitle from "@/components/layout/TopTitle/TopTitle";
 import { prisma } from "@/prisma/prisma";
 
 export default async function MBTITestPage() {
@@ -9,11 +10,16 @@ export default async function MBTITestPage() {
     },
   });
   return (
-    <div className='flex flex-col p-10 gap-6 place-self-center w-dvw items-center justify-center h-dvh relative'>
+    <div className='flex flex-col p-10 gap-10 place-self-center w-dvw items-center justify-center h-dvh relative'>
+      <TopTitle
+        title='آزمون MBTI'
+        h1='تیپ شخصیتی خود را بشناسید'
+        iconName='bot'
+        sub='آزمون تیپ شخصیتی MBTI (Myers-Briggs Type Indicator) یکی از محبوب‌ترین و معتبرترین آزمون‌های روانشناسی است که به شما کمک می‌کند تا بهتر خودتان را بشناسید و نقاط قوت و ضعف شخصیت خود را درک کنید.'
+        containerClassName='mb-4 text-center'
+      />
 
       <div className='flex flex-col gap-5 bg-glass md:p-20 p-3 z-30 rounded-md'>
-        <h2>آزمون تیپ شخصیتی MBTI (Myers-Briggs Type Indicator)</h2>
-
         <LandingExamForum exam={exam} mode='step' />
       </div>
     </div>
